9686f24ae614611efd9f68e6862c9b761a40a689
itsnitinthakur <itsnitinthakur@gmail.com>
2025-08-30 12:45:49 -0500

    better solution for the sum of the two

--- day1_ sum_of_two.py
+++ day1_ sum_of_two.py
@@ -1,2 +1,12 @@
-nums = [2,7,9,11]
-print("nums")
+nums =[2,7,9,11,13]
+target = 9
+h= {}
+for i, num in enumerate(nums):
+    
+    n= target - num
+    print(n)
+    if n not in h:
+        h[num] = i
+    else:
+        print ([h[n], i])
+    print(h)
